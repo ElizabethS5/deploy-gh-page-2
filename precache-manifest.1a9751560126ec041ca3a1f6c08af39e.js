addf3d62c4b7401dcb06322e6c40e9f97849ead5
Elizabeth5 <escheidt5@gmail.com>
2020-08-07 10:23:20 -0400

    Updates

---
+++ precache-manifest.1a9751560126ec041ca3a1f6c08af39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6957ea1600205b09d4539382777a904f",
+    "url": "/deploy-gh-page-2/index.html"
+  },
+  {
+    "revision": "1385373299d3269c4278",
+    "url": "/deploy-gh-page-2/static/css/main.0cb8a1cf.chunk.css"
+  },
+  {
+    "revision": "9d60164b2b924de366d1",
+    "url": "/deploy-gh-page-2/static/js/2.4b3a4e11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-gh-page-2/static/js/2.4b3a4e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1385373299d3269c4278",
+    "url": "/deploy-gh-page-2/static/js/main.912dd2f7.chunk.js"
+  },
+  {
+    "revision": "402224fc4e299f2889a3",
+    "url": "/deploy-gh-page-2/static/js/runtime-main.8089ae92.js"
+  }
+]);
